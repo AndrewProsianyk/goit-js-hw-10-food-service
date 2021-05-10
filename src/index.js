933e015d5fab350b41bbee845d5fede8410f2aac
Andrew <nonameznoone@gmail.com>
2021-05-10 18:14:36 +0300

    markup complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import menuCardsTpl from './templates/menu-cards.hbs';
+import menuData from './menu.json';
+
+
+const markupContainer = document.querySelector('.js-menu');
+const creatingMarkup = menuCardsTpl(menuData);
+
+markupContainer.insertAdjacentHTML('beforeend', creatingMarkup);
