4e4c020a541d8666943a00df22333ac6d263b7b1
Andrew <nonameznoone@gmail.com>
2021-05-11 22:30:49 +0300

    completed hw

--- src/index.js
+++ src/index.js
@@ -47,33 +47,3 @@ function onThemeSwitcherChange() {
     localStorage.setItem('theme', bodyEl.classList)
   }
 };
-
-// const themeCheckbox = document.querySelector('#theme-switch-toggle');
-// themeCheckbox.addEventListener('change', onCheckboxStatusChange);
-// const body = document.querySelector('body');
-
-// if (localStorage.getItem('theme') === null) {localStorage.setItem('theme', Theme.LIGHT);}
-// body.classList = localStorage.getItem('theme');
-// if (localStorage.getItem('theme') === Theme.DARK){ 
-//     themeCheckbox.checked = true; 
-// }
-
-// function onCheckboxStatusChange() {
-//     if (themeCheckbox.checked === true) {
-        
-//         if (body.classList.contains(Theme.LIGHT)){
-//             body.classList.remove(Theme.LIGHT);
-//         }
-//         body.classList.add(Theme.DARK);
-//         themeCheckbox.setAttribute('checked', true);
-//         localStorage.setItem('theme', body.classList);
-//     }
-        
-//     else {
-//         if (body.classList.contains(Theme.DARK)){
-//             body.classList.remove(Theme.DARK);
-//         }
-//         body.classList.add(Theme.LIGHT);
-//         localStorage.setItem('theme', body.classList);
-//     } 
-// }
